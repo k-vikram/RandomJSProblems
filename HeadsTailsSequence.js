aeae3aec16af0939c6ba726239abd2f0d8e22bf0
K. Vikram <30444167+k-vikram@users.noreply.github.com>
2020-08-08 21:52:46 +0530

    update to proper test statement

--- HeadsTailsSequence.js
+++ HeadsTailsSequence.js
@@ -1,41 +1,52 @@
 console.log('<------------------ Start JS Test below!!! ------------------>')
 /*
     Problem Statement: (Dell Prelim Test)
-    'recordString' contains the Heads or tails sequence of coins. 0 is Head; 1is Tails,
-    Find out min. no of flips required to achieve the desired sequence of HTHTH (or THTHT)
-    Here we're doing HTHTH.
+    'recordString' contains the Heads or tails sequence of coins. 0 is Head; 1 is Tails,
+    Find out min. no of flips required to achieve the an alternating sequence of HTHTH or THTHT
+    and deternmine the minimum no of flips required to reach the same(the one which needs lesser flips)
 
 */
 
 
-const invertCoins = (recordString = []) => {
+const invertCoins = (arr = []) => {
 
-    const noOfCoins = recordString.length;
+      // Write your code here
+      const noOfCoins = arr.length;
 
-    if (!(recordString instanceof Array) || noOfCoins === 0 ) {
-        return 'Invalid Input';
-    }
-
-    // set 'currentFace' = 'false' for HTHTH ; 'currentFace' = 'true' for THTHT..
-    let currentFace = false,  totalFlipsNeeded = 0;
-
-    const properSequence = [];
-
-    //O(n) complexity
-    for (let i = 0; i < noOfCoins; i++) {
-        const requiredFace = Number(currentFace);
-        if (recordString[i] !== requiredFace) {
-            totalFlipsNeeded++;
-        }
-        properSequence.push(requiredFace);
-        currentFace = !currentFace;
-    }
-
-    return totalFlipsNeeded;
+      if (noOfCoins === 0) {
+          return 'No of coins is 0';
+      }
+  
+      let HTHFace = false, THTFace = true, totalHTHFlip = 0, totalTHTFlip = 0;
+  
+      for (let i = 0; i < noOfCoins; i++) {
+          if (arr[i] !== Number(HTHFace)) {
+              totalHTHFlip++
+          }  
+          if (arr[i] !== Number(THTFace)) {
+              totalTHTFlip++;
+          }
+          HTHFace = !HTHFace;
+          THTFace = !THTFace;
+      }
+  
+      let maxFlips = 0;
+  
+      if (totalHTHFlip > totalTHTFlip) {
+          maxFlips = totalTHTFlip;
+      } else if (totalHTHFlip < totalTHTFlip) {
+          maxFlips = totalHTHFlip;
+      } else if (totalHTHFlip === totalTHTFlip) {
+          if (totalHTHFlip !== 0) {
+              maxFlips = totalHTHFlip;
+          }
+      }
+  
+      return maxFlips;
 }
 
 // Input type string
-const recordString = [0, 1, 1, 0, 0, 0, 1, 0, 1, 1, 0, 1, 1, 0, 0, 1, 1, 1, 0, 1, 1, 0, 0, 1, 1, 1, 1, 1, 1];
+const recordString = [1, 0, 1, 0, 1, 1, 1, 1, 1];
 
 const res = invertCoins(recordString);
 
