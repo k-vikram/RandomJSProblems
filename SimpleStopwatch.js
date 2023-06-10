e4de94863694a6af568e0906591de4399e5e3666
K. Vikram <30444167+k-vikram@users.noreply.github.com>
2023-06-10 17:56:11 +0530

    Create SimpleStopwatch.js
    React/JS problem to make simplestopwatch

---
+++ SimpleStopwatch.js
@@ -0,0 +1,78 @@
+import "./styles.css";
+import { useEffect, useRef, useState } from "react";
+
+export default function App() {
+  return (
+    <div className="App">
+      <h1>Hello CodeSandbox</h1>
+      <h2>Start editing to see some magic happen!</h2>
+
+      <Stopwatch delay={10} />
+    </div>
+  );
+}
+
+export const Stopwatch = ({ delay }) => {
+  const [timerValue, setTimerValue] = useState(() => Number(delay));
+  let timerObj = useRef(null);
+
+  const startTimer = (e) => {
+    e.preventDefault();
+
+    clearInterval(timerObj.current);
+    if (Number(timerValue) <= 0) {
+      resetTimer(e);
+    }
+
+    timerObj.current = setInterval(() => {
+      setTimerValue((prev) => {
+        return Number(prev - 1);
+      });
+    }, 1000);
+  };
+
+  useEffect(() => {
+    if (Number(timerValue) <= 0) {
+      clearInterval(timerObj.current);
+    }
+
+  }, [timerValue]);
+
+  useEffect(() => {
+    return () => clearInterval(timerObj.current);
+  }, [])
+
+  const pauseTimer = (e) => {
+    e.preventDefault();
+    clearInterval(timerObj.current);
+  };
+
+  const resetTimer = (e) => {
+    e.preventDefault();
+    clearInterval(timerObj.current);
+    setTimerValue(Number(delay));
+  };
+
+  return (
+    <div>
+      <button type="button" onClick={startTimer}>
+        START TIMER
+      </button>
+      <button type="button" onClick={pauseTimer}>
+        PAUSE TIMER
+      </button>
+      <button type="button" onClick={resetTimer}>
+        RESET TIMER
+      </button>
+      <div
+        style={{
+          fontSize: "40px",
+          color: "red",
+          padding: "10px"
+        }}
+      >
+        {timerValue}
+      </div>
+    </div>
+  );
+};
